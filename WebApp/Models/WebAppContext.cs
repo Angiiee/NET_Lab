4243eeadb2b2abc66c488c2c6fa7426c685e7c3a
Anhelina_Maretskaya <Anhelina_Maretskaya@epam.com>
2019-01-04 02:23:05 +0300

    Publish .NET project

---
+++ WebApp/Models/WebAppContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class WebAppContext : DbContext
+    {    
+        public WebAppContext() : base("name=WebAppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebApp.Models.Movie> Movies { get; set; }
+    }
+}
